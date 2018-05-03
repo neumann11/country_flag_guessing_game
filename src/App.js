43e34460759b081dcd46403ea1b3ff8d556c5a4a
neumann11 <pavlo.vlasyuk@gmail.com>
2018-05-03 15:40:11 +0200

    finish app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CountryGame from './CountryGame';
+import worldImg from './world.jpg'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="flag-app">
+        <header
+            className="title-header"
+            style={{ backgroundImage: `url(${worldImg})` }}>
+          <h1 className="title-text">Guess The Flag</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <CountryGame />
       </div>
     );
   }
